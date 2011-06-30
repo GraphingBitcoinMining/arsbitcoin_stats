7a1eeabed25c81b19668faa064d9b1cbe6d95d4f
IceyEC <chmacnaughton@gmail.com>
2011-06-29 21:24:56 -0500

    added workers

--- arsbtcstats.php
+++ arsbtcstats.php
@@ -3,7 +3,7 @@
 $now = time();
 
 include("config.php");
-$db = mysql_connect($host,$user,$password);
+$db = mysql_connect($host,$dbuser,$dbpassword);
 mysql_select_db($database,$db);
 $api_key="7e468d0bda6a6c98cdf7e1173d65c4f315fb6f4937160049dff1af4775ab9db2";
 
@@ -27,19 +27,20 @@ $json_data=json_decode($json_string);
 
 //$confirmed_rewards = $json_data->confirmed_rewards;
 $hashrate = $json_data->hashrate;
+$workers = $json_data->currentworkers;
 //$payout_history = $json_data->payout_history;
 
 //echo $confirmed_rewards;
 echo $hashrate;
-echo '<br><br>';
+echo '<br>'.$workers.'<br>';
 //echo $payout_history;
 
-$sql = "INSERT INTO `arsbtcstats`.`global_stats` (`id`, `time`, `hashrate`) VALUES (NULL, '$now', '$hashrate');";
+$sql = "INSERT INTO `arsbtcstats`.`global_stats` (`id`, `time`, `hashrate`, `workers`) VALUES (NULL, '$now', '$hashrate', '$workers');";
 //$sql = "INSERT INTO `arsbtcstats`.`personal_stats` (`id`, `confirmed_rewards`, `hashrate`, `payout_history`) VALUES (NULL, '$confirmed_rewads', '$hashrate', '$payout_history');";
 $query = mysql_query($sql);
 
 //Personal User Stats
-
+/*
 $json_url = 'http://arsbitcoin.com/api.php?api_key='.$api_key;
 
 $ch = curl_init( $json_url );
@@ -67,7 +68,7 @@ echo '<br>';
 echo $payout_history;
 
 $sql2 = "INSERT INTO `arsbtcstats`.`personal_stats` (`id`, `confirmed_rewards`, `hashrate`, `payout_history`, `time`) VALUES (NULL, '$confirmed_rewards', '$hashrate', '$payout_history', '$now');";
-$query2 = mysql_query($sql2);
+$query2 = mysql_query($sql2); */
 //echo $json_string;
 //echo $json_data->hashrate;
 ?>
--- global_stats.php
+++ global_stats.php
@@ -12,17 +12,20 @@
 
     $request = "SELECT * FROM `global_stats` ORDER BY `id` DESC LIMIT 0,100";
     $result = mysql_query($request,$db);
-    $time_raw=array(); $hashrate_raw=array();
+    $time_raw=array();
+	$hashrate_raw=array();
+	$workers_raw=array();
    while($row = mysql_fetch_array($result))
       {
         $time_raw[] = (int)$row["time"]*1000;
         $hashrate_raw[] = (int)$row["hashrate"]/1000;
+		$workers_raw[] = (int)$row["workers"];
       }
 	  
     //echo $row["time"];
     $time = array_reverse($time_raw);
     $hashrate = array_reverse($hashrate_raw);
-
+	$workers = array_reverse($workers_raw);
 	//for ( $i = 0; $i < sizeof($time); $i++)
 	//{
 	//	$time[$i] = date("Y-m-d H:i:s", $time[$i]);
@@ -34,6 +37,13 @@ function make_pair($time, $hashrate) {
 
 $hasharray = array_map('make_pair', $time, $hashrate);
 $datapoints = json_encode($hasharray);
+
+function make_pair2($time, $workers) {
+    return array($time, $workers);
+}
+
+$hasharray = array_map('make_pair2', $time, $workers);
+$datapoints2 = json_encode($hasharray);
 if ($_GET[debug] == 1)
   {
     echo "Debug enabled!";
@@ -52,9 +62,10 @@ mysql_free_result($result);
 	<script type="text/javascript">
 $(function () {
     var d1 = <?PHP echo $datapoints; ?>;
+	var d2 = <?PHP echo $datapoints2; ?>;
     
     var plot = $.plot($("#placeholder"),
-           [ { data: d1, label: "Hashrate (GH)"} ], {
+           [ { data: d1, label: "Hashrate (GH)"}, { data: d2, label: "Current Workers"} ], {
                series: {
                    lines: { show: true },
                    points: { show: true },
@@ -64,6 +75,7 @@ $(function () {
 						mode: "time",
 						timeformat: "%H:%M<br>%m/%d"
 				 },
+				 
                grid: { hoverable: true, clickable: true }
              });
 
@@ -103,7 +115,7 @@ $(function () {
 					var datetime = hours  + ':' + minutes + '  ' + month + '/' + day;
 				
 				showTooltip(item.pageX, item.pageY,
-							"<center>Hashrate at <br>" + datetime + "<br>= " + y + "GH</center>");
+							"<center>" + item.series.label +" at <br>" + datetime + "<br>= " + y + "</center>");
 			}
 		}
             
