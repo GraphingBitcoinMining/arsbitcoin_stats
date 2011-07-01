13e75f0ee4b555688fe14e16e6797c57f6fb4bce
Christoffer Müller Madsen <lego399@gmail.com>
2011-07-01 19:14:03 +0200

    added block saving to database; added check for already added blocks

--- getblocks.php
+++ getblocks.php
@@ -2,6 +2,22 @@
 include("config.php");
 $json_url = 'http://arsbitcoin.com/blockApi.php';
 
+$db = mysql_connect($host,$dbuser,$dbpassword);
+mysql_select_db($database,$db);
+ 
+	function objectToArray($d) {
+		if (is_object($d)) {
+			$d = get_object_vars($d);
+		}
+		if (is_array($d)) {
+			return array_map(__FUNCTION__, $d);
+		}
+		else {
+			return $d;
+		}
+	}
+ 
+
 $ch = curl_init($json_url);
 $options = array(
   CURLOPT_RETURNTRANSFER => true,
@@ -12,7 +28,30 @@ curl_setopt_array( $ch, $options );
 $json_string = curl_exec( $ch );
 $json_data = json_decode( $json_string );
 
-$blocks = $json_data->blocks;
+$blocks_raw = $json_data->blocks;
+//print_r($blocks);
+//var_dump($blocks[1]);
+$blocks = objectToArray($blocks_raw);
 print_r($blocks);
-var_dump($blocks[1]);
+echo "<br>";
+
+foreach($blocks as $value) {
+  $sql = "SELECT * FROM `blocks` WHERE `id` LIKE {$value['id']}";
+  $query = mysql_query($sql);
+  $exists = @mysql_num_rows($query);
+  
+  if($exists == 0) {
+    echo "INSERT INTO `arsbtcstats`.`blocks` (`id`, `blocknumber`, `timestamp`, `shares`) VALUES ({$value['id']}, {$value['blockNumber']}, {$value['timestamp']}, {$value['shares']});";
+    $sql = "INSERT INTO `arsbtcstats`.`blocks` (`id`, `blocknumber`, `timestamp`, `shares`) VALUES ({$value['id']}, {$value['blockNumber']}, {$value['timestamp']}, {$value['shares']});";
+    $query = mysql_query($sql);
+    
+    echo "Added - Block ".$value['id']." with ".$value['shares']." shares when it was completed at ".date('Y-m-d H:i:s', $value['timestamp']);
+    echo "<br>";
+  }
+  else {
+    echo "Block ".$value['id']." is already in the database.";
+    echo "<br>";
+  }
+}
+mysql_close();
 ?>
