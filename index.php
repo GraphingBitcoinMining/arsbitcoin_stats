7efc2c979d64f3358a4993755fbe127a35a9f772
IceyEC <chmacnaughton@gmail.com>
2011-08-01 11:08:27 -0500

    cleaning up formatting

--- index.php
+++ index.php
@@ -36,7 +36,6 @@ include("memcached.php");
 }
 ?>
 <center><div style="width: 95%;"><div id="container" style="width:1000px;height:600px"></div>
-<div id="network" style="width:1000px;height:300px;"></div>
 
 </div>
 	<?php if ($enable_donation_message == 1) {
